1b30d26051481a921ba09e55109adfaef2685c4d
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2024-02-24 09:59:36 -0500

    changes on the index.css

--- django_projects/mysite/mysite/settings.py
+++ django_projects/mysite/mysite/settings.py
@@ -124,7 +124,7 @@ LOGOUT_REDIRECT_URL = 'tasks/login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tasks/static/')]
 
 
 # Default primary key field type
