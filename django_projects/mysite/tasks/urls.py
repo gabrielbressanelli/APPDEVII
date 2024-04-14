c1c42c7c206425abc8fa9f47fafdce175ce33de2
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2024-04-14 14:45:55 -0400

    New Weather API

--- django_projects/mysite/tasks/urls.py
+++ django_projects/mysite/tasks/urls.py
@@ -1,6 +1,6 @@
 # tasks/urls.py
 from django.urls import path
-from .views import register_view, task_list, add_task, login_view, logout_view, delete_task, index_view, calendar_view
+from .views import register_view, task_list, add_task, login_view, logout_view, delete_task, index_view, calendar_weather
 
 urlpatterns = [
     path('', index_view, name='index'),
@@ -10,6 +10,6 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('delete/<int:task_id>/', delete_task, name='delete_task'),
-    path('calendar/', calendar_view, name='calendar')
+    path('calendar/', calendar_weather, name='calendar')
 
 ]
--- django_projects/mysite/tasks/views.py
+++ django_projects/mysite/tasks/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
@@ -8,6 +10,7 @@ from .forms import ScheduleForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+
 
 
 def register_view(request):
@@ -72,3 +75,35 @@ def index_view(request):
 @login_required()
 def calendar_view(request):
     return render(request,'tasks/calendar.html')
+
+def calendar_weather(request):
+    API_KEY = "def835724653f5a3572e989f637a53de"
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    forecast_url= "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        weather_data = fetch_weather_and_forecast(city, API_KEY, current_weather_url, forecast_url)
+
+        context = {
+            "weather_data": weather_data
+        }
+        return render(request, 'tasks/calendar.html', context)
+    else:
+        return render(request, 'tasks/calendar.html')
+    
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+
+    weather_data = {
+        "city": city,
+        "temperature": round(response['main']['temp'] - 273.15)
+
+
+    }
+
+    return weather_data
+
